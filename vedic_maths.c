e386b9b818e10defb62aaa33b36e5770a1c868a2
anilkhatiwada <anilkhatiwada@gmail.com>
2017-12-29 22:31:35 +0545

    Update vedic_maths.c

--- vedic_maths.c
+++ vedic_maths.c
@@ -1,20 +1,21 @@
+
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
   int a;
-printf("enter a number to begin with\n");
+printf("\t\t\t\t\t enter a number to begin with\n");
 scanf("%d",&a);
-printf("\n\n\n%d\t",a+3);
+printf("\n\n\n\t\t\t\t\t%d\t",a+3);
 printf("%d\t",a+8);
-printf("%d\n",a+1);
+printf("%d\n\t\t\t\t\t",a+1);
 printf("%d\t",a+2);
 printf("%d\t",a+4);
-printf("%d\n",a+6);
+printf("%d\n\t\t\t\t\t",a+6);
 printf("%d\t",a+7);
 printf("%d\t",a);
 printf("%d\n\n\n",a+8);
-printf("you will get result %d while doing addition from any side",a+3+a+8+a+1);
+printf("\t\t\t\t\tyou will get same result (%d) while doing addition from any side",a+3+a+8+a+1);
 getch();
 
 }
