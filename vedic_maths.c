d23692ec67d5fdd6610f831359652c6d2846f6e9
anilkhatiwada <anilkhatiwada@gmail.com>
2017-12-29 22:21:02 +0545

    Add files via upload

---
+++ vedic_maths.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+printf("enter a number to begin with\n");
+scanf("%d",&a);
+printf("\n\n\n%d\t",a+3);
+printf("%d\t",a+8);
+printf("%d\n",a+1);
+printf("%d\t",a+2);
+printf("%d\t",a+4);
+printf("%d\n",a+6);
+printf("%d\t",a+7);
+printf("%d\t",a);
+printf("%d\n\n\n",a+8);
+printf("you will gate result %d while doing addition from any side",a+3+a+8+a+1);
+getch();
+
+}
